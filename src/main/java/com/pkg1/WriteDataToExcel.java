3d6e1a37b174156b8965dabb659f96bb99207ccb
KellyKou <xuetingkou7@gmail.com>
2021-09-19 13:58:32 +1000

    t

---
+++ src/main/java/com/pkg1/WriteDataToExcel.java
@@ -0,0 +1,37 @@
+package com.pkg1;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class WriteDataToExcel {
+    public static void main(String[] args) throws IOException {
+        //
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        XSSFSheet sheet1=workbook.createSheet("S1");
+        Row r0 = sheet1.createRow(0);
+        Cell c0 = r0.createCell(0);
+        c0.setCellValue("kelly");
+
+        //create file
+        File f=new File(System.getProperty("user.dir")+"\\src\\main\\java\\com\\properties\\TestData\\TestData.xls");
+        FileOutputStream fileOutputStream = new FileOutputStream(f);
+        //Write into work book
+        workbook.write(fileOutputStream);
+        //close workbook &
+        fileOutputStream.close();
+        workbook.close();
+
+        System.out.println("success");
+
+
+
+
+    }
+
+}
